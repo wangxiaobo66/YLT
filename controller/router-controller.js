62f0726a4cbe33ae3ee1d2827659fc96700def25
wangxiaobo <wangxiaobo@hbc315.com>
2016-08-25 22:47:25 +0800

    commit

---
+++ controller/router-controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wangxiaobo on 16/8/25.
+ */
+/**路由设置**/
+var views = require('co-views');
+var render = views(__dirname,{map: {html: 'swig'}});
+
+module.exports = {
+    index: function *(next){
+        this.body = yield render('../template/index.html');
+    }
+}
