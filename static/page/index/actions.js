9ce1336b9a259fe525b3c928882f0519d12e678c
wangxiaobo <wangxiaobo@hbc315.com>
2016-09-04 15:23:19 +0800

    commit

--- static/page/index/actions.js
+++ static/page/index/actions.js
@@ -1,3 +1,13 @@
 /**
  * Created by wangxiaobo on 16/9/4.
  */
+function indexNum(num){
+    return {
+        type: INDEX_NUM,
+        indexNum
+    }
+}
+
+module.exports = {
+    INDEX_NUM:INDEX_NUM
+};
--- static/page/index/index.js
+++ static/page/index/index.js
@@ -1,3 +1,41 @@
 /**
  * Created by wangxiaobo on 16/9/4.
  */
+const React = require('react');
+const render = require('react-dom').render;
+
+
+const { YLT } = require('../../redux/reducers');
+const { Provider, connect } = require('react-redux');
+const { createStore, applyMiddleware } = require('redux');
+const thunk = require('redux-thunk').default;
+
+let store = createStore(YLT, applyMiddleware(thunk));
+
+class component extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>123</div>
+        );
+    }
+    componentDidMount() {
+        let {dispatch} = this.props;
+    }
+}
+function select(state) {
+    console.log(state)
+    return {
+        index: state.index
+    }
+}
+let Index = connect(select)(component);
+
+render(
+    <Provider store={store}>
+        <Index />
+    </Provider>,
+    document.getElementById("index")
+);
