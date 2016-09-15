a7adfcb17812337a4eeb29c46c55119c246b8058
wangxiaobo <wangxiaobo@hbc315.com>
2016-09-15 11:21:59 +0800

    commit

--- static/page/login/login.js
+++ static/page/login/login.js
@@ -3,11 +3,9 @@
  */
 require('./login.scss');
 require('../../scss/base.scss');
+const util = require('../../js/app/util.js');
 const React = require('react');
 const render = require('react-dom').render;
-
-const img_mobile = require('../../images/mobile.png');
-import img_sms from './img/sms.png';
 
 const { YLT } = require('../../redux/reducers');
 const { Provider, connect } = require('react-redux');
@@ -15,41 +13,100 @@ const { createStore, applyMiddleware } = require('redux');
 const thunk = require('redux-thunk').default;
 
 let store = createStore(YLT, applyMiddleware(thunk));
+let t;
 
 class component extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            text: '登陆',
+            number: 60,
+            value: {mobile: '', sms: ''},
+            active: false,
+            after: false
+        }
     }
+
     render() {
+        let { text , active , value ,after} = this.state;
         return (
             <div className="module-login">
+                <img className="background" src="../../static/page/login/img/background.jpg"/>
                 <div className="portrait">
                     <div className="portrait-div">
-                        <img src=""/>
+                        <img src="../../static/page/login/img/logo.png"/>
                     </div>
-                    <p className="portrait-p">Joker</p>
+                    {/*<p className="portrait-p">Joker</p>*/}
                 </div>
                 <div className="verify">
                     <div className="verify-mobile">
-                        {/* 方式一: 使用js引入 */}
-                        <img src={img_mobile}/>
-                        <input type="tel"/>
-                        <span>获取</span>
+                        <img src="../../static/page/login/img/mobile.png"/>
+                        <input type="tel" placeholder="请输入手机号" onChange={(e) => this.onchange(e)} value={value.mobile}
+                               maxLength="11"/>
+                        <span onClick={active?(after?(e) => this.getSms():''):''}
+                              className={active?(after?'active':'after'):''}>{text}</span>
                     </div>
                     <div className="verify-sms">
-                        {/* 相对于login.html这个页面进行相对路径搜索对应图片 */}
-                        <img src="../../static/images/sms.png"/>
-                        {/* 模块化img */}
-                        <img src={img_sms} alt=""/>
-                        <input type="text"/>
+                        <img src="../../static/page/login/img/sms.png"/>
+                        <input type="text" placeholder="请输入短信验证码"/>
+                    </div>
+                    <a href="javascript:;" className="verify-submit">登录</a>
+                    <div className="others">
+                        <p>其它方式登录</p>
+                        <img src="../../static/page/login/img/wx.png"/>
                     </div>
                 </div>
             </div>
         );
     }
+
+    getSms() {
+        t = setTimeout(function () {
+            _this.getSms();
+        }, 1000);
+        let { text,number } = this.state;
+        let _this = this;
+        if (number === 0) {
+            clearTimeout(t);
+            this.setState({
+                text: '重新获取',
+                after: true,
+                number: 60
+            })
+        } else {
+            number--;
+            text = number + 's';
+            this.setState({
+                text: text,
+                number: number,
+                after: false
+            })
+        }
+    }
+    onchange(e) {
+        let {value} = this.state;
+        let val = e.target.value;
+        value.mobile = val;
+        this.setState({
+            value: value
+        });
+        this.mobile(val);
+    }
+    mobile(val) {
+        let {active,after}=this.state;
+        if (/^(13|15|18)\d{9}$/.test(val)) {
+            active = true;
+            after = true
+        } else {
+            active = false;
+        }
+        this.setState({
+            active: active,
+            after: after
+        });
+    }
 }
 function select(state) {
-    console.log(state);
     return {}
 }
 let Login = connect(select)(component);
