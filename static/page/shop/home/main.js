7a72720e585a9e6a8f9cebd7e9c63e0930b4152c
jinguangguo <jinguangguo@new4g.cn>
2016-09-17 12:08:13 +0800

    fxibug

---
+++ static/page/shop/home/main.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @auth jinguangguo
+ * @date 2016/9/17
+ */
+
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+export default class Chepihao extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        return(
+            <div className="module-shop-add">
+                店铺首页
+            </div>
+        );
+    }
+}
