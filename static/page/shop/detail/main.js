e6ba1cd75c9ff4976d1a3e2a71ae045f6e5435be
jinguangguo <jinguangguo@new4g.cn>
2016-09-19 21:00:57 +0800

    店铺详情

--- static/page/shop/detail/main.js
+++ static/page/shop/detail/main.js
@@ -7,6 +7,8 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 
+import imgLogo from '../img/logo.png';
+
 export default class Chepihao extends React.Component {
     constructor(props) {
         super(props);
@@ -20,7 +22,27 @@ export default class Chepihao extends React.Component {
     render() {
         return(
             <div className="module-shop-detail">
-                详情
+                <div className="header">
+                    <div className="img">
+                        <img src={imgLogo} width="60" height="60" alt=""/>
+                    </div>
+                    <div className="info">
+                        <p className="title">这里是公司名称</p>
+                        <p className="sub-title">这里是副标题</p>
+                        <p className="care">关注人数: 21</p>
+                    </div>
+                </div>
+                <div className="tabs clearfix">
+                    <div className="item">
+                        <p className="num">21</p>
+                        <p className="tip">全部商品</p>
+                    </div>
+                    <div className="divide">|</div>
+                    <div className="item">
+                        <p className="num">22</p>
+                        <p className="tip">最新商品</p>
+                    </div>
+                </div>
             </div>
         );
     }
--- webpack.entry.js
+++ webpack.entry.js
@@ -1,7 +1,6 @@
 /**
  * @file
  * @auth jinguangguo
- * @date 2016/8/31
  */
  
 module.exports = {
