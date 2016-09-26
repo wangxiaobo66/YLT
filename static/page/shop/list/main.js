9ec080ef9c4091542f681b803591350b3f56a012
jinguangguo <jinguangguo@new4g.cn>
2016-09-26 22:42:36 +0800

    fix完善页面-店铺

--- static/page/shop/list/main.js
+++ static/page/shop/list/main.js
@@ -6,9 +6,7 @@
 
 import React from 'react';
 import {Link} from 'react-router';
-
-import imgLogo from '../img/logo.png';
-import imgE1 from '../img/example.png';
+import Shop from '../../../component/Shop/Shop';
 
 export default class Chepihao extends React.Component {
     constructor(props) {
@@ -44,70 +42,7 @@ export default class Chepihao extends React.Component {
                     </label>
                 </div>
                 <div className="list-box">
-                    <ul className="list">
-                        <li className="item">
-                            <Link className="item-link" to={`/home`}>
-                                <div className="item-info">
-                                    <img className="img" src={imgLogo} width="45" height="45" alt=""/>
-                                    <div className="info-detail">
-                                        <div className="title">
-                                            <div className="title-location">
-                                                <i className="icon icon-location"></i>
-                                                <span className="text">满洲里</span>
-                                            </div>
-                                            <div className="title-name">公司名称</div>
-                                        </div>
-                                        <div className="sub-title">这里暂时是副标题</div>
-                                        <div className="desc">公司介绍内容</div>
-                                    </div>
-                                </div>
-                                <div className="goods-box">
-                                    <ul className="goods clearfix">
-                                        <li className="item">
-                                            <img src={imgE1} width="123" height="63" alt=""/>
-                                        </li>
-                                        <li className="item">
-                                            <img src={imgE1} width="123" height="63" alt=""/>
-                                        </li>
-                                        <li className="item">
-                                            <img src={imgE1} width="123" height="63" alt=""/>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </Link>
-                        </li>
-                        <li className="item">
-                            <Link className="item-link" to={`/home`}>
-                                <div className="item-info">
-                                    <img className="img" src={imgLogo} width="45" height="45" alt=""/>
-                                    <div className="info-detail">
-                                        <div className="title">
-                                            <div className="title-location">
-                                                <i className="icon icon-location"></i>
-                                                <span className="text">满洲里</span>
-                                            </div>
-                                            <div className="title-name">公司名称</div>
-                                        </div>
-                                        <div className="sub-title">这里暂时是副标题</div>
-                                        <div className="desc">公司介绍内容</div>
-                                    </div>
-                                </div>
-                                <div className="goods-box">
-                                    <ul className="goods clearfix">
-                                        <li className="item">
-                                            <img src={imgE1} width="123" height="63" alt=""/>
-                                        </li>
-                                        <li className="item">
-                                            <img src={imgE1} width="123" height="63" alt=""/>
-                                        </li>
-                                        <li className="item">
-                                            <img src={imgE1} width="123" height="63" alt=""/>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </Link>
-                        </li>
-                    </ul>
+                    <Shop />
                 </div>
                 <Link className="ui-btn ui-btn-fixed" to={`/add`}>我要开店</Link>
             </div>
